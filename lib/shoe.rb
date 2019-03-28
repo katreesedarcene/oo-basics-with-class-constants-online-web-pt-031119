0c95783db7b4a664174b4cf4c3c05c3201bab2ed
Kat B <katreesebarnes@gmail.com>
2019-03-28 16:10:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
    
-   BRANDS = ["Uggs", "Rainbow", "Nike"]
+   BRANDS.size
 
   def initialize(brand)
     @brand = brand
@@ -15,7 +15,7 @@ class Shoe
   def brand=(brand)
     @brand = brand 
     BRANDS << brand 
-    BRANDS.size
+    
   end
 
 end
